8b629fb126687286e8cc764da08bea8e8e27d958
fearlessdinosaur <c15436172@mydit.ie>
2019-05-30 12:26:13 +0100

    added insertion sorts - insertion sort

--- Sorts.py
+++ Sorts.py
@@ -13,3 +13,22 @@ def Bubble(x):
                 x[j] = x[j-1]
                 x[j-1] = temp
                 print(x)
+
+def insertion(x):
+    print(x)
+    for i in range(1,len(x)):
+        y = x[i]
+        print("key:"+str(y))
+        z = i-1
+
+        while(z >=0 and x[z] > y):
+            x[z+1] = x[z]
+            z = z - 1
+
+        x[z+1] = y
+        print(x)
+
+
+            
+            
+        
